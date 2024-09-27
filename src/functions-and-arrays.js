025a26c32f2d959f19958897342a8f82c0aefbf5
Andrea Bronzoni <bronzoni.andrea@libero.it>
2024-09-27 22:15:27 +0200

    LAB | JS Functions & Arrays - completed

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,5 +1,16 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(num1, num2) {
+	if (num1 > num2) {
+		return num1;
+	}
+	else if (num1 < num2) {
+		return num2;
+	}
+	else {
+		return num1;
+	}
+	
+}
 
 
 
@@ -7,7 +18,19 @@ function maxOfTwoNumbers() {}
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
+function findLongestWord(listOfWords) {
+	let longestWord='';
+	if (listOfWords.length === 0) {
+		return null;
+	}
+	
+	listOfWords.forEach(function(word) {
+		if (word.length > longestWord.length) {
+		longestWord = word;
+		}
+	});
+	return longestWord;
+}
 
 
 
@@ -15,15 +38,27 @@ function findLongestWord() {}
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
-
+function sumNumbers(numbers) { 
+    let sum = 0;
+    
+    numbers.forEach(function(num){
+    sum += num;
+    });
+        return sum;
+    };
 
 
 
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(numbers) {
+   if (numbers.length===0) { 
+	return 0; 
+}
+    return sumNumbers(numbers)/numbers.length;
+}
+
 
 
 
@@ -31,4 +66,9 @@ function averageNumbers() {}
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist() {}
+function doesWordExist(words, searchedWord) {
+	if (words.length ===0) {
+		return null;
+	}
+	return words.includes(searchedWord);
+}
